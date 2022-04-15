7322058223ec1411b96c95dcdd5e468b03c5050f
tindn <hi@tindn.io>
2022-04-15 12:26:42 -0400

    display game pieces on the board

--- src/components/Square/component.tsx
+++ src/components/Square/component.tsx
@@ -1,3 +1,5 @@
+import { useAppSelector } from "../../store/hooks";
+import { Color } from "../../types";
 import "./styles.css";
 
 interface Props {
@@ -5,6 +7,17 @@ interface Props {
   row: number;
   column: number;
 }
+
 export function Square(props: Props) {
-  return <div className={`square col-${props.column} row-${props.row} `}></div>;
+  // transparent meant there's no game piece in the square
+  const gamePieceColor =
+    useAppSelector(
+      (state): Color | undefined => state.app.gamePieces[props.index.toString()]
+    ) || "transparent";
+
+  return (
+    <div className={`square col-${props.column} row-${props.row}`}>
+      <div className={`gamepiece color-${gamePieceColor}`} />
+    </div>
+  );
 }
